67d1112437a89b2ad723670566e36636125cff26
Nobeul <nobeul.cse@gmail.com>
2024-05-01 17:24:57 +0600

    fixed subtotal of invoice receipt

--- resources/views/sale_pos/receipts/classic.blade.php
+++ resources/views/sale_pos/receipts/classic.blade.php
@@ -474,7 +474,7 @@
 						<td class="text-right">
 
 							
-						    {{ number_format( pregReplaceFloat($receipt_details->total) - (float)$totalTax, 2) }}
+						    {{ number_format( pregReplaceFloat($receipt_details->total) - round($receipt_details->vat, 2), 2) }}
 							
 							{{-- @if (! empty($receipt_details->taxes))
 						    	{{(float)preg_replace('/[^0-9.]/', '', $receipt_details->total) - (float)preg_replace('/[^0-9.]/', '', $receipt_details->taxes[array_key_last($receipt_details->taxes)])}}
